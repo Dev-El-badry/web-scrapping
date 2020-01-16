8eda252083cd89ccc9ca30dc691bdf3f30f2a821
DevElbadry <miliarder96@gmail.com>
2020-01-16 23:58:07 +0200

    finish install databse mongodb

--- souq_project/souq_project/pipelines.py
+++ souq_project/souq_project/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import pymongo
 
+class MongodbPipeline(object):
+    collection_name = 'seller_items'
 
-class SouqProjectPipeline(object):
+    def open_spider(self, spider) :
+        self.client = pymongo.MongoClient("mongodb+srv://eslam:eslamelbadry@cluster0-62be9.mongodb.net/test?retryWrites=true&w=majority")
+        self.db = self.client["SOUQ"]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
